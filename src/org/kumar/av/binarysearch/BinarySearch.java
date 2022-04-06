c4b575cd3a754567cfa2c4def28bc84544bc20dd
Saket Kumar <saket0207@gmail.com>
2022-04-06 13:15:42 +0530

    binary search

---
+++ src/org/kumar/av/binarysearch/BinarySearch.java
@@ -0,0 +1,37 @@
+package org.kumar.av.binarysearch;
+
+import java.util.Scanner;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter size");
+        int size = sc.nextInt();
+        int [] arr = new int[size];
+        System.out.println("Enter elements in sorted form");
+        for(int i = 0; i < arr.length; i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Enter number to search");
+        int n = sc.nextInt();
+        int index = binarySearchCustom(arr, n);
+        System.out.println(index);
+
+    }
+
+    private static int binarySearchCustom(int[] arr, int n) {
+
+        int start = 0, end = arr.length - 1;
+        while (start <= end){
+            int mid = start + (end - start)/2;
+            if(n == arr[mid]){
+                return mid;
+            }else if(n < arr[mid]){
+                end = mid - 1;
+            }else{
+                start = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
